a19253d1c7956505fb26becae40f5f00bbe62ab2
plaxswap <plaxswap@gmail.com>
2026-05-21 21:13:58 +0800

    first commit

--- apps/bridge/components/stargate/config.ts
+++ apps/bridge/components/stargate/config.ts
@@ -1,10 +1,10 @@
 import { arbitrum, mainnet, optimism, polygon, avalanche, fantom } from 'wagmi/chains'
 
 const VERSION = '0.0.33'
-const SHA384 = 'RDYGBMTG+YS5OF8Kavau0Xdyq6j7e/5bFMF55lYu3Oz3gthIOqQSSJkcz96n6knF'
+const SHA384 = '021ubXIhzewAwp2U/18P+WBqyCb6BviBfQGTvZOpHQi3h5fiNknvYAwiz9mS04Th'
 export const PARTNER_ID = 0x0002
 export const FEE_COLLECTOR = '0xc6F09C01b4C213932907D1CC56144f301DeaD153'
-export const FEE_TENTH_BPS = '10'
+export const FEE_TENTH_BPS = '0'
 
 export const STARGATE_JS = {
   src: `https://unpkg.com/@layerzerolabs/stargate-ui@${VERSION}/element.js`,
