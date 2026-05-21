deabee587041586f55f0a8679585cc39c3743a65
plaxswap <plaxswap@gmail.com>
2026-05-21 20:08:32 +0800

    first commit

--- apps/web/src/views/Bridge/StargateWidget.tsx
+++ apps/web/src/views/Bridge/StargateWidget.tsx
@@ -1,36 +1,54 @@
 import { Box, PancakeTheme } from '@pancakeswap/uikit'
+import { darkColors, lightColors } from '@pancakeswap/ui/tokens/colors'
 import { useMemo } from 'react'
 import { FEE_COLLECTOR, FEE_TENTH_BPS, PARTNER_ID } from './config'
 
-const getWidgetTheme = (theme: PancakeTheme) => ({
-  palette: {
-    type: theme.isDark ? 'dark' : 'light',
-    primary: {
-      main: theme.colors.primary,
-      contrastText: theme.colors.invertedContrast,
+const getWidgetTheme = (theme: PancakeTheme) => {
+  const colors = theme.isDark ? darkColors : lightColors
+
+  return {
+    palette: {
+      type: theme.isDark ? 'dark' : 'light',
+      primary: {
+        main: colors.primary,
+        contrastText: colors.invertedContrast,
+      },
+      info: {
+        main: colors.primary,
+        light: colors.invertedContrast,
+      },
+      success: {
+        main: colors.success,
+      },
+      error: {
+        main: colors.failure,
+      },
+      warning: {
+        main: colors.warning,
+      },
+      text: {
+        primary: colors.text,
+        secondary: colors.textSubtle,
+        disabled: colors.textDisabled,
+      },
+      divider: colors.cardBorder,
+      background: {
+        paper: colors.backgroundAlt,
+        default: colors.background,
+      },
+      action: {
+        disabled: colors.textDisabled,
+        disabledBackground: colors.disabled,
+      },
     },
-    text: {
-      primary: theme.colors.text,
-      secondary: theme.colors.textSubtle,
-      disabled: theme.colors.textDisabled,
+    typography: {
+      fontFamily: "'Kanit', sans-serif",
     },
-    divider: theme.colors.cardBorder,
-    background: {
-      paper: theme.colors.backgroundAlt,
-      default: theme.colors.background,
+    shape: {
+      borderRadius: 18,
     },
-    action: {
-      disabled: theme.colors.textDisabled,
-      disabledBackground: theme.colors.disabled,
-    },
-  },
-  typography: {
-    fontFamily: "'Kanit', sans-serif",
-  },
-  shape: {
-    borderRadius: 18,
-  },
-})
+  }
+}
 
 const StargateWidget = ({ theme }: { theme: PancakeTheme }) => {
   const widgetTheme = useMemo(() => JSON.stringify(getWidgetTheme(theme)), [theme])
