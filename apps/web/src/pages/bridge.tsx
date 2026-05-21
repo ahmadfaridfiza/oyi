4fda26cecac5c242a9b75eb1edb8c99ab7405e5c
plaxswap <plaxswap@gmail.com>
2026-05-22 03:29:17 +0800

    first commit

--- apps/web/src/pages/bridge.tsx
+++ apps/web/src/pages/bridge.tsx
@@ -1,7 +1,10 @@
 import Bridge from '../views/Bridge'
+import { BRIDGE_CHAINS } from '../config/constants/bridgeChains'
 
 const BridgePage = () => {
   return <Bridge />
 }
+
+BridgePage.chains = BRIDGE_CHAINS.map((chain) => chain.id)
 
 export default BridgePage
